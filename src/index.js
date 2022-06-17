0db7c5ed171f6c83225b83d1f3af9ef76fca6916
angelo <angelo.hizon27@gmail.com>
2022-06-17 22:34:44 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//const React = require('react');
+//const ReactDOM = require('react-dom');
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>My name is Giovanni Giorgio but you can call me... Giorgio.</p>
+  </div>,
+  document.getElementById("root")
+);
